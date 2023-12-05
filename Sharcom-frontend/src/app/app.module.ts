e1f538acb36cbfd75f57d7d4085f9ee777fd1195
428780 <robbie@vanhoof-erp.nl>
2023-12-05 10:19:31 +0100

    Initial setup & Auth0

--- Sharcom-frontend/src/app/app.module.ts
+++ Sharcom-frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './components/authButton/authButton.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+        domain: 'sharcom.eu.auth0.com',
+        clientId: 'i3wUmGYhowyE9LDKiufnO2zEtp9s9srI',
+        authorizationParams: {
+            redirect_uri: window.location.origin
+        }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
